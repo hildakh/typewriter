f951affb8c6240bdb8190c0b3b08083b355ec3aa
hildakh <h_khajehamiri@yahoo.com>
2019-10-22 20:59:01 +0000

    using timeout to print words with delay

---
+++ hello_timeout.js
@@ -0,0 +1,17 @@
+const words = ['4311o', 'th3r3', 'w0r1d'];
+// for (const word in words) {
+//   setTimeout(() => {
+//   }, 5000);
+//   console.log(word);
+// }
+
+setTimeout (() => {
+  console.log('4311o')
+}, 2000);
+setTimeout (() => {
+  console.log('th3r3')
+}, 3000);
+
+setTimeout (() => {
+  console.log('thw0r1d3r3')
+}, 4000);
